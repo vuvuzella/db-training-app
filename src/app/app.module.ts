b52de7148594441e8a7fa4d0a43c317d66846918
vuvuzella <j.tabacii@gmail.com>
2017-07-10 01:27:42 +1000

    Added Flexbox, angular material icons and set app margin to 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,26 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 import { AppRoutingModule } from './app-routing.module'
 
+// Application-wide Module imports
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
+
+import { MdToolbarModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, // Entry component for future modules to be added to this web app
     NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     DashboardModule,
     AppRoutingModule,
+    FlexLayoutModule,
+    MaterialModule,
+    MdToolbarModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
